4e9e5b45629359c6fa89d2f656da009bf14ae305
saminsakur <saminsakur8@gmail.com>
2021-05-19 21:51:23 +0600

    created buttons on button.py

--- buttons.py
+++ buttons.py
@@ -2,12 +2,18 @@ from tkinter import *
 
 
 window = Tk() # the class for the window
+
 # function to exit:
 def exit():
     window.destroy()
-    
+
+
+################
+myButton = Button(window, text="Click me")
+myButton.pack()
+
 # Create a buton to exit:
-Button(window, width=12, command=exit, bg="red", text="Close", fg="black").grid(row=3, column=0)
+Button(window, width=12, command=exit, bg="red", text="Close", fg="black").pack() #.grid(row=0, column=0)
 
 # main loop to show the window repeatedly maybe
 window.mainloop()
