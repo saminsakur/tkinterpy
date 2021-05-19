d50129b6328e4b73c48b6140b35b557495158009
saminsakur <saminsakur8@gmail.com>
2021-05-19 21:44:31 +0600

    updated grid.py

--- grid.py
+++ grid.py
@@ -1,16 +1,17 @@
 from tkinter import *
 
+
 window = Tk() # the class for the window
+# function to exit:
+def exit():
+    window.destroy()
 
-# create a label widget
-text1 = Label(window, text="Hello world!")
-text2 = Label(window, text="My name is Samin Sakur")
-text3 = Label(window, text="I'm a student of class 7")
+# create a label widget and gridding 
+Label(window, text="Hello world!").grid(row=0, column=0)
+Label(window, text="My name is Samin Sakur").grid(row=1, column=1)
 
-# gridding labels
-text1.grid(row=0, column=0)
-text2.grid(row=1, column=1)
-text3.grid(row=2, column=3)
+# Create a buton to exit:
+Button(window, width=12, command=exit, bg="red", text="Close", fg="black").grid(row=3, column=0)
 
 # main loop to show the window repeatedly maybe
 window.mainloop()
