d6f76e3bba2636100b13b65796ebbea6bfa44045
saminsakur <saminsakur8@gmail.com>
2021-05-19 17:06:17 +0600

    created new column

--- grid.py
+++ grid.py
@@ -4,10 +4,13 @@ window = Tk() # the class for the window
 
 # create a label widget
 text1 = Label(window, text="Hello world!")
+text2 = Label(window, text="My name is Samin Sakur")
+text3 = Label(window, text="I'm a student of class 7")
 
-# Shoving it onto the window 
-text1.pack()
-
+# gridding labels
+text1.grid(row=0, column=0)
+text2.grid(row=1, column=1)
+text3.grid(row=2, column=3)
 
 # main loop to show the window repeatedly maybe
 window.mainloop()
