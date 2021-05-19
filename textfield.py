329e9d85541a4310bae5d9793f86f02462652a37
saminsakur <saminsakur8@gmail.com>
2021-05-19 22:32:06 +0600

    initial commit

--- textfield.py
+++ textfield.py
@@ -6,18 +6,22 @@ window = Tk() # the class for the window
 # ses window tile
 window.title("textfield.py")
 
-e = Entry(window, width=50)
-e.pack()
-content = e.get()
+
 # function to exit:
 def exit():
     window.destroy()
 
 # function sayHello for myButton:
 def sayHello():
-    Label(window, text="Hello," + e.get()).pack()
+    Label(window, text="Hello, " + e.get()).pack()
+# Create a Label
+Label(window, text="Enter your name")
 
-################
+#Create a entry widget 
+e = Entry(window, width=50)
+e.pack()
+
+# Create a submit button
 myButton = Button(window, text="SUBMIT", command=sayHello)
 myButton.pack()
 
