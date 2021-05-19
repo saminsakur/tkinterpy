bbb414d8f8f59dfcbb53c2e481c648005d275ef1
saminsakur <saminsakur8@gmail.com>
2021-05-19 16:03:35 +0600

    updated simple.py

--- simple.py
+++ simple.py
@@ -1,6 +1,11 @@
 from tkinter import *
 
 window = Tk()
+# sets the window title
+window.title("My gui")
+
+# sets window logo / image
+window.iconmask()
 
 # main loop to display the window repeatedly
 window.mainloop()
